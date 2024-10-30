2a3c77df89973913050f99d0edc9998a3218295e
MitchSultan <admin@mitch.sultan.theotherguyske.org>
2024-10-31 00:33:53 +0300

    homereredset

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 
+import { StrictMode } from 'react'
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,16 +23,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <StrictMode>
+    <>
     <html lang="en">
       
         
       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
       <Analytics/>
     </html>
+    </>
+    </StrictMode>
   );
 }
